9a7d0242a669528ecfe8988ca859ea8c5e2ed7d3
Avinash Dhanuka <118340660+Avinash-706@users.noreply.github.com>
2025-06-25 11:35:58 +0530

    First Commit

--- API/index.js
+++ API/index.js
@@ -1,10 +1,50 @@
-let mysql = require("./connection.js");
 let express = require("express");
 let bodyParser = require("body-parser"); //bodyparser is the inbuilt middleware in nodejs
+const connection = require("./connection.js");
+const port = 3000;
 
 let app = express();
 app.use(bodyParser.json()); // Convert incoming data into json
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+
+//----------------------
+
+app.get("/", (req, res) => {
+    // your logic here
+    res.send("Get Response is Working!!!");
+});
+
+app.get("/api/category", (req, res) =>{
+    connection.query("SELECT id, topic_name, description, is_enable FROM master_category", (err, result)=>{
+    if(err){
+        res.status(400).json({msg: "Error in sql"});
+    }
+    else{
+        res.status(200).json({msg: result})
+    }
 })
+})
+
+app.get("/api/category/:id", (req, res) => {
+    const categoryId = req.params.id; //params dyanmanic changes in a website
+
+    connection.query("SELECT id, topic_name, description, is_enable FROM master_category WHERE id = ?", [categoryId], (err, result) => {
+            if (err) {
+                res.status(400).json({ msg: "Error in SQL" });
+            }
+            else if (result.length === 0) {
+                res.status(404).json({ msg: "Category not found" });
+            }
+            else {
+                res.status(200).json({ category: result[0] });
+            }
+        });
+});
+
+
+//----------------------id, cate_name, description, is_enable
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
